0e5aff6a02d852a3305751a3f45b5627d50f1cd3
Marko Heini <marko.heini@gmail.com>
2014-11-03 18:15:43 +0200

    Initial commit

---
+++ gamething/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  gamething
+//
+//  Created by Marko Heini on 03/11/14.
+//  Copyright (c) 2014 mh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+
+    return 0;
+}
