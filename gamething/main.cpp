974cadd3459f2fce398082bc69a9db01fdd31d27
Marko Heini <marko.heini@gmail.com>
2014-11-03 18:36:25 +0200

    Created project with libs

--- gamething/main.cpp
+++ gamething/main.cpp
@@ -8,11 +8,72 @@
 
 #include <iostream>
 
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+static void error_callback(int error, const char* description)
+{
+    fputs(description, stderr);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
+
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    GLFWwindow* pWindow;
+    glfwSetErrorCallback(error_callback);
+
+    if (!glfwInit()) {
+        exit(EXIT_FAILURE);
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, 4);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    pWindow = glfwCreateWindow(640, 480, "window", NULL, NULL);
+    if (!pWindow) {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwMakeContextCurrent(pWindow);
+    glewExperimental = GL_TRUE;
+
+    // Initialize GLEW
+    GLenum rc = glewInit();
+    if (rc != GLEW_OK) {
+        fprintf(stderr, "GLEW error: '%s'\n", glewGetErrorString(rc));
+        exit(EXIT_FAILURE);
+    }
+
+    // Ensure we can capture the escape key
+    glfwSetInputMode(pWindow, GLFW_STICKY_KEYS, GL_TRUE);
+
+    // Enable depth test
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+
+    glfwSetKeyCallback(pWindow, key_callback);
+
+    const GLubyte* renderer = glGetString (GL_RENDERER); // get renderer string
+    const GLubyte* version = glGetString (GL_VERSION); // version as a string
+
+    std::cout << "Renderer: " << renderer << "\n";
+    std::cout << "OpenGL version supported: " << version << "\n";
+    std::cout << "Shader lang: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << "\n";
+
+    // TODO: stuff here..
 
     return 0;
 }
